8310ed281af867d7568eb8bd4c1d5b4fd62a7719
Maxim Mitin <maximus405@gmail.com>
2021-11-16 01:14:04 +0300

    init

--- ARkittest2/ViewController.swift
+++ ARkittest2/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ARSCNViewDelegate {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sceneView.delegate = self
+        sceneView.debugOptions = [.showFeaturePoints]
+        
+        if let scene = SCNScene(named: "art.scnassets/ship.scn") {
+            sceneView.scene = scene
+        } 
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
 
 }
 
